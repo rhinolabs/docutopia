7b0a0870cb89de5dfd499b88c51a5eea18231a34
GabrielaDiazB <gabriela.dbravo@gmail.com>
2025-01-15 15:15:19 -0500

    feat: add new dependencies and configs to run project

---
+++ tailwind.config.ts
@@ -0,0 +1,87 @@
+import type { Config } from "tailwindcss";
+import tailwindcssAnimate from "tailwindcss-animate";
+
+const config: Config = {
+	darkMode: ["class"],
+	content: [
+		"./src/**/*.{ts,tsx}",
+		"components/**/*.{ts,tsx}",
+		"./node_modules/@shadcn/ui/**/*.js",
+		"./node_modules/@rhino-ui/ui/**/*.{js,jsx,ts,tsx}",
+	],
+	theme: {
+		container: {
+			center: true,
+			padding: "2rem",
+			screens: {
+				"2xl": "1400px",
+			},
+		},
+		extend: {
+			colors: {
+				background: "hsl(var(--background))",
+				foreground: "hsl(var(--foreground))",
+				card: {
+					DEFAULT: "hsl(var(--card))",
+					foreground: "hsl(var(--card-foreground))",
+				},
+				popover: {
+					DEFAULT: "hsl(var(--popover))",
+					foreground: "hsl(var(--popover-foreground))",
+				},
+				primary: {
+					DEFAULT: "hsl(var(--primary))",
+					foreground: "hsl(var(--primary-foreground))",
+				},
+				secondary: {
+					DEFAULT: "hsl(var(--secondary))",
+					foreground: "hsl(var(--secondary-foreground))",
+				},
+				muted: {
+					DEFAULT: "hsl(var(--muted))",
+					foreground: "hsl(var(--muted-foreground))",
+				},
+				accent: {
+					DEFAULT: "hsl(var(--accent))",
+					foreground: "hsl(var(--accent-foreground))",
+				},
+				destructive: {
+					DEFAULT: "hsl(var(--destructive))",
+					foreground: "hsl(var(--destructive-foreground))",
+				},
+				border: "hsl(var(--border))",
+				input: "hsl(var(--input))",
+				ring: "hsl(var(--ring))",
+				chart: {
+					"1": "hsl(var(--chart-1))",
+					"2": "hsl(var(--chart-2))",
+					"3": "hsl(var(--chart-3))",
+					"4": "hsl(var(--chart-4))",
+					"5": "hsl(var(--chart-5))",
+				},
+				sidebar: {
+					DEFAULT: "hsl(var(--sidebar-background))",
+					foreground: "hsl(var(--sidebar-foreground))",
+					primary: "hsl(var(--sidebar-primary))",
+					"primary-foreground": "hsl(var(--sidebar-primary-foreground))",
+					accent: "hsl(var(--sidebar-accent))",
+					"accent-foreground": "hsl(var(--sidebar-accent-foreground))",
+					border: "hsl(var(--sidebar-border))",
+					ring: "hsl(var(--sidebar-ring))",
+				},
+				get: "hsl(var(--get-color))",
+				post: "hsl(var(--post-color))",
+				put: "hsl(var(--put-color))",
+				delete: "hsl(var(--delete-color))",
+				patch: "hsl(var(--patch-color))",
+				head: "hsl(var(--head-color))",
+				options: "hsl(var(--options-color))",
+				connect: "hsl(var(--connect-color))",
+				trace: "hsl(var(--trace-color))",
+			},
+		},
+	},
+	plugins: [tailwindcssAnimate],
+};
+
+export default config;
