23b47419ed486e3f4161decc6f7588d39b8f4b82
Jhonn Gutierrez <jhonngutierrez.contact@gmail.com>
2026-01-13 16:14:05 -0500

    lint fix

--- apps/docs/astro.config.mjs
+++ apps/docs/astro.config.mjs
@@ -1,18 +1,18 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
-import tailwindcss from '@tailwindcss/vite';
+import tailwindcss from "@tailwindcss/vite";
 
-import react from '@astrojs/react';
+import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  vite: {
-    plugins: [tailwindcss()],
-    ssr: {
-      noExternal: ['@docutopia/react']
-    }
-  },
-  output: 'server',
-  integrations: [react()]
+	vite: {
+		plugins: [tailwindcss()],
+		ssr: {
+			noExternal: ["@docutopia/react"],
+		},
+	},
+	output: "server",
+	integrations: [react()],
 });
